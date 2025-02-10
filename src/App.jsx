6174b4e39ca1fb2bd428cdd4ba2d98b5ba1b9e50
Sareen Ahamed <sareenahamed40@gmail.com>
2025-02-11 02:56:04 +0530

    Edit option Initialized

--- src/App.jsx
+++ src/App.jsx
@@ -89,7 +89,6 @@ const App = () => {
   //For Edit
   
     
-
   return (
     <div className='min-h-screen bg-gray-100 p-6'>
       <div className='max-w-2xl mx-auto bg-white p-6 rounded-lg shadow-md'>
